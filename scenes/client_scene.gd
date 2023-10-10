198c4afd5595f7a237d023cd9a4f65a280272845
Leo Warde Hunter <71136113+VinoPate@users.noreply.github.com>
2023-10-10 16:03:57 +0100

    Improved Client Scene

--- scenes/client_scene.gd
+++ scenes/client_scene.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var price_setting = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,7 +9,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$dialogue_window/dialogue_text.visible_characters += 1
+	
+	if $dialogue_window/dialogue_text.visible_ratio == 1:
+		$dialogue_window/Button.visible = true
 
 
 func _on_gold_slider_value_changed(value):
@@ -17,3 +21,9 @@ func _on_gold_slider_value_changed(value):
 
 func _on_button_pressed():
 	$gold_bar.value += $gold_slider.value
+
+
+func _on_button_pressed_2():
+	$dialogue_window/dialogue_text.text = 'Set your price for the reading.'
+	$dialogue_window/dialogue_text.visible_ratio = 0
+	$gold_slider.visible = true
