a5ea5dc0d7c46879ac2b2a24a4085c503328ab75
Leo Warde Hunter <71136113+VinoPate@users.noreply.github.com>
2023-10-27 16:04:25 +0100

    Client Art Added
    -Fixed the reading scene and reparented the threed_card_root node to a viewport container so that it would render ontop of the scene

--- scenes/reading_scene.gd
+++ scenes/reading_scene.gd
@@ -1,11 +1,11 @@
 extends Node3D
 
 signal finish_reading_scene(selection)
+@onready var threed_card_root = $SubViewportContainer/SubViewport/threed_card_root
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -15,7 +15,8 @@ func _process(delta):
 func _on_d_card_root_card_selected():
 	$reading_scene.visible = true
 	$reading_scene._new_card()
-	$threed_card_root.visible = false
+	threed_card_root.visible = false
+	$SubViewportContainer.visible = false
 
 
 func _on_client_scene__card_reading_options(option_a, option_b):
@@ -24,17 +25,19 @@ func _on_client_scene__card_reading_options(option_a, option_b):
 
 
 func _on_client_scene__card_selection_ready():
-	$threed_card_root._reset()
+	threed_card_root._reset()
 	visible = true
 	$reading_scene.visible = false
-	$threed_card_root.visible = true
-	$threed_card_root.click_ready = true
+	threed_card_root.visible = true
+	threed_card_root.click_ready = true
+	$SubViewportContainer.visible = true
 
 
 func _on_option_a_pressed():
 	finish_reading_scene.emit('a')
 	print('reading a')
 	$reading_scene.visible = false
+	
 
 
 func _on_option_b_pressed():
